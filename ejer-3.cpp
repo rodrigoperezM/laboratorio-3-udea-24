58d4a9766a39f3a0e207d83a99a1023d65d39c91
rodrigoperezM <96254619+rodrigoperezM@users.noreply.github.com>
2024-04-07 21:41:54 -0500

    Add files via upload
    Dificultad al compilar.

---
+++ ejer-3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "caracter_y_secuencia.h"
+#include<stdlib.h>
+#include <fstream>
+#include <string>
+
+
+int main()
+{
+	string nom_archivo;
+	char caracter;
+	string secuencia2;
+	
+	cout << "Ingrese el nombre del archivo: ";
+	cin >> nom_archivo;
+	cout << "Que caracter quier buscar: ";
+	cin >> caracter;
+	cout << "la secuencia de texto a buscar es : ";
+	cin >> secuencia2;
+	
+	leer(nom_archivo, caracter, secuencia2);
+
+	return 0;
+}
